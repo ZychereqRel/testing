fbf658ac4dfe37aadcd0d547ef753e1304996c22
ZychereqRel <93724868+ZychereqRel@users.noreply.github.com>
2024-02-11 14:43:47 +0100

    i forgor

--- app.py
+++ app.py
@@ -124,4 +124,5 @@ def search():
     return render_template('search.html', result=result_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=port, host='0.0.0.0')
